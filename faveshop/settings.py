a751322aab7104d38916bf0fede54b279695fba0
Favour Ronald Enu <156347682+techyfave@users.noreply.github.com>
2025-12-29 23:45:40 +0100

    Add Whitenoise storage for static files

--- faveshop/settings.py
+++ faveshop/settings.py
@@ -150,6 +150,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Crispy Forms
